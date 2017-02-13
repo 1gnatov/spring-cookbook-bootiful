fc56aa53714bd1eb9b428e01229c47e694650acd
1gnatov <ignatov.msu@gmail.com>
2017-02-13 16:41:51 +0300

    converting app to scheduled1

--- src/main/java/org/test/bookpub/StartupRunner.java
+++ src/main/java/org/test/bookpub/StartupRunner.java
@@ -3,12 +3,12 @@ package org.test.bookpub;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.test.bookpub.repository.BookRepository;
 
 import javax.sql.DataSource;
 
-public class StartupRunner implements CommandLineRunner {
+public class StartupRunner {
 	private final Log logger = LogFactory.getLog(getClass());
 
 	@SuppressWarnings({"SpringAutowiredFieldsWarningInspection", "SpringJavaAutowiringInspection"})
@@ -18,9 +18,8 @@ public class StartupRunner implements CommandLineRunner {
 	@Autowired
 	private BookRepository bookRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		//logger.info("DataSource: " + ds.toString());
+	@Scheduled(initialDelay = 1000, fixedRate = 10000)
+	public void run() throws Exception {
 		logger.info("Number of books: " + bookRepository.count());
 	}
 
