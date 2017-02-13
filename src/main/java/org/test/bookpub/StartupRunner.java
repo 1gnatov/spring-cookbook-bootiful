4ce797ee65dd19d17145bf2ef66688dee8264be1
1gnatov <ignatov.msu@gmail.com>
2017-02-13 15:21:39 +0300

    added command-line runner

---
+++ src/main/java/org/test/bookpub/StartupRunner.java
@@ -0,0 +1,13 @@
+package org.test.bookpub;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
+
+public class StartupRunner implements CommandLineRunner {
+	private final Log logger = LogFactory.getLog(getClass());
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Hello world!");
+	}
+}
