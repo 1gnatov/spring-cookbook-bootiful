7e7e9bc0ad7a9718ea54a9e8f2de1c0243716ac3
1gnatov <ignatov.msu@gmail.com>
2017-02-13 15:49:57 +0300

    autowired datasource from h2

--- src/main/java/org/test/bookpub/StartupRunner.java
+++ src/main/java/org/test/bookpub/StartupRunner.java
@@ -2,12 +2,21 @@ package org.test.bookpub;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+
+import javax.sql.DataSource;
 
 public class StartupRunner implements CommandLineRunner {
 	private final Log logger = LogFactory.getLog(getClass());
+
+	@SuppressWarnings({"SpringAutowiredFieldsWarningInspection", "SpringJavaAutowiringInspection"})
+	@Autowired
+	private DataSource ds;
+
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("Hello world!");
+		logger.info("DataSource: " + ds.toString());
 	}
+
 }
